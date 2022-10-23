b1ed907cdcaae3a28bd5716bab8de609feb752d8
Rajat Raghuwanshi <77507423+rajat10cube@users.noreply.github.com>
2022-10-23 14:41:54 -0400

    updated some files

--- polls/tests.py
+++ polls/tests.py
@@ -3,7 +3,9 @@ from django.test import TestCase
 from django.utils import timezone
 
 from .models import Question
+
 # Create your tests here.
+
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
